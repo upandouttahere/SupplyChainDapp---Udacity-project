26fb06920a3c621692851c62501e7ad742fd441d
Laurette <laurette@gmx.net>
2020-01-28 20:34:58 +0100

    Add files via upload

--- truffle-config.js
+++ truffle-config.js
@@ -21,33 +21,28 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const infuraKey = "6a480df17fc474da0d0e782520b038d";
 
-const fs = require('fs');
+// const fs = require('fs');
 const mnemonic = "emotion member desert above uncover miracle happy bring seek cake elevator high";
 
-
 module.exports = {
-  contracts_build_directory: "./client/src/contracts",
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    // rinkeby: {
+      // provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      // network_id: 4,
+      // gas: 4500000,
+      // gasPrice: 10000000000
+    // },
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
       network_id: 4,
-      gas: 5500000,        // Rinkeby has a lower block limit than mainnet
-      gasPrice: 21000000000,
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      gas: 5000000,
+      gasPrice: 10000000000,
+      skipDryRun: true,
     },
-  },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: "0.5.1" // Fetch exact version from solc-bin (default: truffle's version)
-    }
   }
-}
+};
